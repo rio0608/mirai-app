430ea66ab819ad8a636fe194dd83411cb7b5288f
rio0608 <6051_2024_06508@g.miyazaki-c.ed.jp>
2026-06-04 14:35:55 +0900

    GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import {fileURLToPath} from 'url';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  const rootDir = fileURLToPath(new URL('.', import.meta.url));
+
   return {
+    base: '/mirai-app/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': rootDir,
       },
     },
     server: {
